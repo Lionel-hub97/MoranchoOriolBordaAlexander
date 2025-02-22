35b756cb1a13b25bc562c4d836602db5008edaad
albc9 <abordach108@ub.edu.cat>
2025-02-22 23:09:47 +0100

    Camping delcaradas sus metodos pero no que hace, el error de la calsse gestpr camping prosigue, ns como hacer que no de error, no detecta la classe camping

--- .idea/src/prog2/model/Camping.java
+++ .idea/src/prog2/model/Camping.java
@@ -1,9 +1,22 @@
 package prog2.model;
 import java.util.*;
 public class Camping {
-    int x = 5;
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Como te llamas");
+    public void afegirParcela(String nom, String idAllotjament, float metres, boolean connexioElectrica) {
     }
+
+    public void afegirBungalow(String nom, String idAllotjament, String mida, int habitacions, int placesPersones, int placesParquing, boolean terrassa, boolean tv, boolean aireFred) {
+    }
+
+    public void afegirBungalowPremium(String nom, String idAllotjament, String mida, int habitacions, int placesPersones, int placesParquing, boolean terrassa, boolean tv, boolean aireFred, boolean serveisExtra, String codiWifi) {
+    }
+
+    public void afegirGlamping(String nom, String idAllotjament, String mida, int habitacions, int placesPersones, String material, boolean casaMascota) {
+    }
+
+    public void afegirMobilHome(String nom, String idAllotjament, String mida, int habitacions, int placesPersones, boolean terrassaBarbacoa) {
+    }
+
+    public void afegirClient(String nomClient, String dni) throws ExcepcioReserva {
+    }
+
 }
